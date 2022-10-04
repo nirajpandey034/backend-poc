0740ae8cefa8d805b769d7954c4c83bcaf67c2f8
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2022-10-04 12:47:13 +0530

    removed swagger files

--- swagger.js
+++
@@ -1,8 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')()
-
-const outputFile = './swagger_output.json'
-const endpointsFiles = ['./index.js']
-
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
-    require('./index.js')
-})
