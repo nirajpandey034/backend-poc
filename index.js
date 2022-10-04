d232f6d4b81957f2924598e3ce373faff5bc0c6a
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2022-10-04 12:38:17 +0530

    added swagger for dev

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('dotenv').config();
 const DBConnect = require('./config/database');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger_output.json');
 
 const productRoute = require('./routes/productsRoute');
 const userRoute = require('./routes/usersRoute');
@@ -12,6 +14,13 @@ app.use(express.json());
 
 DBConnect(); //connection to db
 
+if (process.env.NODE_ENV === 'DEV') {
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} else {
+  app.use('/docs', (req, res) => {
+    res.send('This operation is not supported in this environment.');
+  });
+}
 app.use('/products', productRoute);
 app.use('/users', userRoute);
 
@@ -19,9 +28,9 @@ app.post('/welcome', auth, (req, res) => {
   res.status(200).send('Welcome 🙌 ');
 });
 
-app.use('/', function (req, res, next) {
-  res.send('Home Route');
-});
+// app.use('/', function (req, res, next) {
+//   res.send('Home Route');
+// });
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
